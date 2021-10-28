224d49c9096e415922205433ed77d4207c141998
kiaruh <nikoz.li@gmail.com>
2021-10-28 01:19:00 -0300

    cors

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 const db = require('./db.json');
 let port = process.env.PORT || 3001
 
+const cors = require('cors')
+
+app.use(cors())
+
+
 app.get('/',(req,res)=>{
     res.send(
         {
@@ -15,3 +20,5 @@ app.get('/',(req,res)=>{
 app.get('/products', (req, res) => {res.send(db)})
 
 app.listen( port ,() => console.log(`Server start http://localhost:${port}`))
+
+
