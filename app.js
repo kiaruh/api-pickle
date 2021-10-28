3b0e80c0ca71f4ac87aea03067396832ba980878
kiaruh <nikoz.li@gmail.com>
2021-10-27 23:30:27 -0300

    fix2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const db = require('./db.json');
 let port = process.env.PORT || 3001
 
 app.get('/',(req,res)=>{
-    res.status(200).send(
+    res.send(
         {
             product:'/products'
         }
@@ -11,6 +11,6 @@ app.get('/',(req,res)=>{
 
 )
 
-app.get('/products', (req, res) => {res.status(200).send(db)})
+app.get('/products', (req, res) => {res.send(db)})
 
 app.listen( port ,() => console.log(`Server start http://localhost:${port}`))
