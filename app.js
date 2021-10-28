cb5c77db718a052bbbd55931070ba407eb4d33cf
kiaruh <nikoz.li@gmail.com>
2021-10-27 23:09:25 -0300

    cambio

--- app.js
+++ app.js
@@ -5,4 +5,13 @@ app.set('port',process.env.PORT || 3001)
 
 app.listen(app.get('port'),() => console.log('Server start http://localhost:' +app.get('port')))
 
+app.get('/',(req,res)=>{
+    res.status(200).send(
+        {
+            product:'/products'
+        }
+    )}
+
+)
+
 app.get('/products', (req, res) => {res.status(200).send(db)})
