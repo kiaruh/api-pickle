add8b5d1359a0a672eab9c9aee281da5a2310a3b
kiaruh <nikoz.li@gmail.com>
2021-10-28 23:38:37 -0300

    fix

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
-const express = require('express');
-const app = express();
-const db = require('./db.json');
-let port = process.env.PORT || 3001
+// const express = require('express');
+// const app = express();
+// const db = require('./db.json');
+// let port = process.env.PORT || 3001
 
-const cors = require('cors')
+// const cors = require('cors')
 
-app.use(cors({origin: "*"}))
+// app.use(cors({'Access-Control-Allow-Origin': "*"}))
+
+// app.get('/',(req,res)=>{
+//     res.send(
+//         {
+//             product:'/products'
+//         }
+//     )}
+
+// )
+// app.get('/products', (req, res) => {res.send(db)})
+// app.listen( port ,() => console.log(`Server start http://localhost:${port}`))
 
 
-app.get('/',(req,res)=>{
-    res.send(
-        {
-            product:'/products'
-        }
-    )}
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-)
+server.use(middlewares);
+server.use(router);
 
-app.get('/products', (req, res) => {res.send(db)})
-
-app.listen( port ,() => console.log(`Server start http://localhost:${port}`))
-
+server.listen(port);
 
